3fc29215d38824740d4d7765ab3d0b1e115cc54d
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2024-01-22 01:15:25 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	stack<int> s;
+	string input;
+	int num;
+
+	while (n--) {
+		cin >> input;
+
+		if (input == "push") {
+			cin >> num;
+			s.push(num);
+		}
+		else if (input == "pop") {
+			if (s.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << s.top() << '\n';
+				s.pop();
+			}
+		}
+		else if (input == "size") {
+			cout << s.size() << '\n';
+		}
+		else if (input == "empty") {
+			cout << s.empty() << '\n';
+		}
+		else if (input == "top") {
+			if (s.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << s.top() << '\n';
+			}
+		}
+	}
+
+	return 0;
+}
